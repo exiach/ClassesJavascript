19b8d69dc44bb5e39dc2180423e80fd494ff5aae
Daniel Canqui <you.nightmare.00@gmail.com>
2021-07-13 11:03:17 -0400

    How to use DOM to render an element in html by javascript class day 7.

---
+++ index.js
@@ -0,0 +1,20 @@
+//const addH1 = () => {
+    //}
+// const element = document.getElementById('app');
+// console.log(element);
+// element.innerHTML = '<h1>Hello DOM document object model</h1>';
+
+//
+function addTitle() {
+    // get element html.
+    const element = document.getElementById('app');
+    // Print in console innerHTML to know what it is the contain.
+    console.log(element.innerHTML)
+    // DOM Modify the html through JavaScript.
+    element.innerHTML = '<h1>Hello DOM document object model</h1>';
+}
+
+//
+
+
+//
